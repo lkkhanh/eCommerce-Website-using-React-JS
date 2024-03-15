204c98dbf0ec94708d4f1c373100fcfd97e5eca5
lkkhanh2000 <1851050060khanh@ou.edu.vn>
2024-03-15 15:11:14 +0700

    b

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -3,62 +3,76 @@ import { Link } from 'react-router-dom';
 import logo from "../assets/images/logo/logo.png"
 
 const NavItems = () => {
-    const [menuToggle, setMenuToggle] = useState (false);
+    const [menuToggle, setMenuToggle] = useState(false);
     const [socialToggle, setSocialToggle] = useState(false);
     const [headerFixed, setHeaderFixed] = useState(false);
 
     // addevent listener
     window.addEventListener("scroll", () => {
-        if (window.scrollY > 200){
+        if (window.scrollY > 200) {
             setHeaderFixed(true);
-        }else{
+        } else {
             setHeaderFixed(false);
         }
     })
-  return (
-    <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp" : ""}`}>
-        {/* header to top start */}
-        <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
-            <div className="container">
-                <div className="header-top-area">
-                    <Link to="/signup" className='lab-btn me-3'><span>Creat Account</span></Link>
-                    <Link to="/login">Log in</Link>
-                </div>
-            </div>
-        </div>
-
-        {/* header botton */}
-
-        <div className="header-bottom">
-            <div className="container">
-                <div className="header-wrapper">
-                    {/* logo */}
-                    <div className="logo-search-acte">
-                        <div className="logo">
-                            <Link to={"/"}>
-                                <img src={logo} alt="" />
-                            </Link>
-                        </div>
-                    </div>
-                    {/* menu area */}
-                    <div className="menu-area">
-                        <div className="menu">
-                            <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
-                                <li><Link to="/">Home</Link></li>
-                                <li><Link to="/shop">Shop</Link></li>
-                                <li><Link to="/blog">Blog</Link></li>
-                                <li><Link to="/about">About</Link></li>
-                                <li><Link to="/contact">Contact</Link></li>
-                            </ul>
-
-                            <Link to="sign-up" className='lab-btn me-3 d-none d-md-block'>Creat Account</Link>
-                        </div>
+    return (
+        <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp" : ""}`}>
+            {/* header to top start */}
+            <div className={`header-top d-md-none ${socialToggle ? "open" : ""}`}>
+                <div className="container">
+                    <div className="header-top-area">
+                        <Link to="/signup" className='lab-btn me-3'><span>Creat Account</span></Link>
+                        <Link to="/login">Log in</Link>
                     </div>
                 </div>
             </div>
-        </div>
-    </header>
-  )
+
+            {/* header botton */}
+
+            <div className="header-bottom">
+                <div className="container">
+                    <div className="header-wrapper">
+                        {/* logo */}
+                        <div className="logo-search-acte">
+                            <div className="logo">
+                                <Link to={"/"}>
+                                    <img src={logo} alt="" />
+                                </Link>
+                            </div>
+                        </div>
+                        {/* menu area */}
+                        <div className="menu-area">
+                            <div className="menu">
+                                <ul className={`lab-ul ${menuToggle ? "active" : ""}`}>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="/shop">Shop</Link></li>
+                                    <li><Link to="/blog">Blog</Link></li>
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/contact">Contact</Link></li>
+                                </ul>
+                            </div>
+
+                            {/* sign & log in */}
+                            <Link to="sign-up" className='lab-btn me-3 d-none d-md-block'>Create Account</Link>
+                            <Link to="/login" className='d-none d-md-block'>Log In</Link>
+
+                            {/* menu toggler */}
+                            <div onClick={() => setMenuToggle(!menuToggle)} className={`header-bar d-lg-none ${menuToggle ? "active" : ""}`}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+
+                            {/* social togger */}
+                            <div className='ellepsis-bar d-md-none' onClick={() => setSocialToggle(!socialToggle)}>
+                                <i className="icofont-info-square"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default NavItems
